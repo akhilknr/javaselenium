4bc9f8844325974f175518106072559abe535f8a
akhilknr <akhil2info4@gmail.com>
2018-02-07 11:05:12 +0530

    First Commit

---
+++ src/automationFramework/FirstTestCase.java
@@ -0,0 +1,25 @@
+package automationFramework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTestCase {
+public static void main(String[] args) throws InterruptedException {
+		
+	
+		WebDriver driver = new FirefoxDriver();
+		
+       
+		driver.get("http://www.store.demoqa.com");
+ 
+        
+        System.out.println("Successfully opened the website www.Store.Demoqa.com");
+ 
+		
+		Thread.sleep(5000);
+		
+        
+        driver.quit();
+    }
+
+}
