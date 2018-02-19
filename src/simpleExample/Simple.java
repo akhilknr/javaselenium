08b929d594c8aad9f615563428aafb12f0a0584e
akhilknr <akhil2info4@gmail.com>
2018-02-19 11:22:41 +0530

    First change

--- src/simpleExample/Simple.java
+++ src/simpleExample/Simple.java
@@ -2,7 +2,7 @@ package simpleExample;
 
 public class Simple {
 public static void main(String[] args) {
-	System.out.println("Simple");
-	//initial 
+	System.out.println("first change");
+	//first change
 }
 }
